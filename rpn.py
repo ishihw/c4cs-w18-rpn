1365005050d062e255ed3f913dfa4fd8fd99fd73
ishihw <378902837@qq.com>
2018-03-06 16:45:34 -0500

    before hw7

--- rpn.py
+++ rpn.py
@@ -1,16 +1,61 @@
 #!/usr/bin/env python3
+
+import operator
+import logging 
+import math
+
 def calculate(arg):
 	stack = list()
+	mod=1
 	for token in arg.split():
 		try:
 			value = int(token)
 			stack.append(value)
 		except ValueError:
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		print (stack)
+			if token =='+':
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = arg1 + arg2
+				stack.append(result)
+			elif token =='-':
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = arg2 - arg1
+				stack.append(result)
+			elif token =='*':
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = arg1 * arg2
+				stack.append(result)
+			elif token =='/':
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = arg2 / arg1
+				stack.append(result)
+			elif token =='sin':
+				arg1 = stack.pop()
+				if(mod==2):
+					arg1=arg1*math.pi/180
+				stack.append(math.sin(arg1))
+			elif token =='cos':
+				arg1 = stack.pop()
+				if(mod==2):
+					arg1=arg1*math.pi/180
+				stack.append(math.cos(arg1))
+			elif token =='rad':
+				mod=1
+			elif token =='deg':
+				mod=2
+			elif token =='cpy':
+				arg1 = stack.pop()
+				stack.append(arg1)
+				stack.append(arg1)
+			else:
+				print('Not an operator')
+		#print (stack)
+	if len(stack) !=1:
+		raise TypeError
+
 	return stack.pop()
 	
 def main():
--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,24 @@ class TestBasics(unittest.TestCase):
 	def test_add2(self):
 		result = rpn.calculate("1 1 + 2 +")
 		self.assertEqual(4,result)
+	def test_sub(self):
+		result = rpn.calculate("4 1 -")
+		self.assertEqual(3,result)
+	def test_red(self):
+		with self.assertRaises(TypeError):
+			result =rpn.calculate("1 2 3 +")
+	def test_mul(self):
+		result = rpn.calculate("1 1 *")
+		self.assertEqual(1,result)
+	def test_div(self):
+		result = rpn.calculate("4 1 /")
+		self.assertEqual(4,result)
+	def test_trigo(self):
+		result = rpn.calculate("0 cos")
+		self.assertEqual(1,result)
+	def test_anglemode(self):
+		result = rpn.calculate("deg 90 sin rad 0 cos +")
+		self.assertEqual(2,result)
+	def test_cpy(self):
+		result = rpn.calculate("1 cpy -")
+		self.assertEqual(0,result)
